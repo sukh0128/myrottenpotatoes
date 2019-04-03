96ec83e4f6d48f3fe1e44887b6eb48d3ecaea327
Erdenesukh Tsendjav <erdenesukh.tsendjav@mnsu.edu>
2019-04-03 21:17:28 +0000

    Setion 4.7 Completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,9 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
-  def index
-    @movies = Movie.all
-  end
+    
+    def index
+        @movies = Movie.all
+    end
   
   
   # in app/controllers/movies_controller.rb
@@ -12,5 +13,16 @@ class MoviesController < ApplicationController
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.html.haml by default
     end
+    
+    def new 
+        @movie = Movie.new
+        # default: render 'new' template
+    end
+    
+    def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
 end
 
